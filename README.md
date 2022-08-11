d524103ab7834d2216246c7837faacc8cd00c4e7
Florian Walter <89262088+fw10@users.noreply.github.com>
2022-08-11 12:31:31 +0200

    Added functionality to zip up a directory recursively and omitting node_modules

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # veracode-node-packager
-Simple auto-packager for Node.js that creates a zip to upload to the Veracode Platform for Static Analysis 
+Simple auto-packager for Node.js that creates a zip to upload to the Veracode Platform for Static Analysis. Please note that this tool is not officially supported by Veracode and there is no warranty whatsoever.
 
 # How to run
 - Via `go run src/main.go`
+- How to build..
+- Releases?
 
 # Test Projects
 - To test this packager, we have used `The node.js example app` from `https://github.com/contentful/the-example-app.nodejs`
--- src/main.go
+++ src/main.go
@@ -1,7 +1,85 @@
 package main
 
-import "fmt"
+import (
+	"archive/zip"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+    "strings"
+)
 
 func main() {
-    fmt.Println("Hello, world.")
+	log.Println("Node Packager - Started")
+
+	folderToZip := "test-projects/my-node-test" // "../test-projects/the-example-app.nodejs-master"
+	outputZip := "output.zip"
+
+	log.Println("Attempting to zip the source")
+	if err := zipSource(folderToZip, outputZip); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Finished zipping")
+}
+
+func zipSource(source, target string) error {
+	// 1. Create a ZIP file and zip.Writer
+	f, err := os.Create(target)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	writer := zip.NewWriter(f)
+	defer writer.Close()
+
+	// 2. Go through all the files of the source
+	return filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// 3. Create a local file header
+		header, err := zip.FileInfoHeader(info)
+		if err != nil {
+			return err
+		}
+
+		// set compression
+		header.Method = zip.Deflate
+
+		// 4. Set relative path of a file as the header name
+		header.Name, err = filepath.Rel(filepath.Dir(source), path)
+		if err != nil {
+			return err
+		}
+
+        // ignore `node_modules`
+        if strings.Contains(header.Name, "node_modules") {
+            return nil
+        }
+
+		if info.IsDir() {
+			header.Name += "/"
+		}
+
+		// 5. Create writer for the file header and save content of the file
+		headerWriter, err := writer.CreateHeader(header)
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		f, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+
+		_, err = io.Copy(headerWriter, f)
+		return err
+	})
 }
