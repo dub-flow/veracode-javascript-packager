8066654cfb4dac9216331299c6358705e688df26
Florian Walter <89262088+fw10@users.noreply.github.com>
2022-08-12 13:09:36 +0200

    Extended build script

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
+rm -r releases
+mkdir -p releases
+
 # build for Windows
 GOOS=windows GOARCH=amd64 go build
-mv vc-node-packager.exe releases/windows-amd64
+mv vc-node-packager.exe releases/vc-node-packager-windows
 
 # build for Mac M1 (arm64)
 #GOOS=darwin GOARCH=arm64 go build
 go build
-mv vc-node-packager releases/mac-arm64
+mv vc-node-packager releases/vc-node-packager-mac-arm64
 
 # build for 64bit Linux (amd64)
 GOOS=linux GOARCH=amd64 go build
-mv vc-node-packager releases/linux-amd64
+mv vc-node-packager releases/vc-node-packager-linux-amd64
