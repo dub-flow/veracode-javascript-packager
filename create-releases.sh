fb812003c9a6fd0a55bc7f9ea96811c0b12ecf68
Florian Walter <89262088+fw10@users.noreply.github.com>
2022-08-17 11:07:03 +0200

    Excluded Mac from my releases as I don't have a developer ID and I don't want to encourage bypassing Apple's security checks

--- create-releases.sh
+++ create-releases.sh
@@ -5,10 +5,10 @@ mkdir -p releases
 GOOS=windows GOARCH=amd64 go build
 mv vc-node-packager.exe releases/vc-node-packager-windows
 
-# build for Mac M1 (arm64)
-#GOOS=darwin GOARCH=arm64 go build
-go build
-mv vc-node-packager releases/vc-node-packager-mac-arm64
+# # build for Mac M1 (arm64)
+# GOOS=darwin GOARCH=arm64 go build
+# go build
+# mv vc-node-packager releases/vc-node-packager-mac-arm64
 
 # build for 64bit Linux (amd64)
 GOOS=linux GOARCH=amd64 go build
