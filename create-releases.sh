a91937f60f0fab7d38b0f34e1f30b36319c5c32e
Florian Walter <89262088+fw10@users.noreply.github.com>
2022-11-17 16:06:41 +0100

    Changed compilation to reduce binary size

--- create-releases.sh
+++ create-releases.sh
@@ -2,14 +2,14 @@ rm -r releases
 mkdir -p releases
 
 # build for Windows
-GOOS=windows GOARCH=amd64 go build
-mv vc-node-packager.exe releases/vc-node-packager-windows
+GOOS=windows GOARCH=amd64 go build -ldflags="-s -w"
+mv vc-node-packager.exe releases/vc-node-packager-windows-amd64
 
 # # build for Mac M1 (arm64)
 # GOOS=darwin GOARCH=arm64 go build
-# go build
+# go build 
 # mv vc-node-packager releases/vc-node-packager-mac-arm64
 
 # build for 64bit Linux (amd64)
-GOOS=linux GOARCH=amd64 go build
+GOOS=linux GOARCH=amd64 go build -ldflags="-s -w"
 mv vc-node-packager releases/vc-node-packager-linux-amd64
