38f4097520e8441cbe332c5bf57d981dc87f8217
Florian Walter <89262088+fw10@users.noreply.github.com>
2023-01-10 11:47:50 +0100

    Included Intel Mac build to create-releases.sh

--- create-releases.sh
+++ create-releases.sh
@@ -5,10 +5,14 @@ mkdir -p releases
 GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -trimpath
 mv veracode-js-packager.exe releases/veracode-js-packager-windows-amd64.exe
 
-# build for Mac M1 (arm64)
-GOOS=darwin GOARCH=arm64 go build -ldflags="-s -w" -trimpath
-mv veracode-js-packager releases/veracode-js-packager-mac-arm64
-
 # build for x64 Linux (amd64)
 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -trimpath
 mv veracode-js-packager releases/veracode-js-packager-linux-amd64
+
+# build for M1 Macs (arm64)
+GOOS=darwin GOARCH=arm64 go build -ldflags="-s -w" -trimpath
+mv veracode-js-packager releases/veracode-js-packager-mac-arm64
+
+# build for Intel Macs (amd64)
+GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -trimpath
+mv veracode-js-packager releases/veracode-js-packager-mac-amd64
